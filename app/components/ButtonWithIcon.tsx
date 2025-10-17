2e69a43923c79e56e8c09fb0b1f7aee394fb809b
tianxu <aoxiaotian@foxmail.com>
2025-10-17 15:21:53 +0800

    feat(优化带icon按钮组件): 实现icon部分

--- app/components/ButtonWithIcon.tsx
+++ app/components/ButtonWithIcon.tsx
@@ -6,10 +6,20 @@ interface ButtonWithIconProps {
   iconUrl?: string;
 }
 
+
 const ButtonWithIcon: React.FC<ButtonWithIconProps> = ({ text, iconUrl }) => {
   return (
     <div>
-          <button className={styles.linkButton}>{text}</button>
+      <button className={styles.linkButton}>
+        {iconUrl && (
+          <img
+            src={iconUrl}
+            alt="icon"
+            style={{ width: 20, height: 20, marginRight: 8, verticalAlign: 'middle' }}
+          />
+        )}
+        {text}
+      </button>
     </div>
   );
 };
